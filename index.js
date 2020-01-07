6a1559404932c33a48dcc8c1bb4e8ce3dd62c034
Tina Hang <mstinahang@gmail.com>
2020-01-07 04:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  document.querySelector("p#text").innerHTML = "This is really cool!";
 });
